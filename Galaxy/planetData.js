c5e798d665d240c8dd67ec452a746085abd31a03
stanLy490 <321684298@qq.com>
2025-12-09 14:33:10 +0800

    12.9_Github released
    https://stanly490.github.io/Portfolio_Website/MainPage/Main.html

--- Galaxy/planetData.js
+++ Galaxy/planetData.js
@@ -10,27 +10,27 @@ const planetInfos = [
             "             Mostly done alone. Used C# and Python.",
             "Status: Still in Progress......"
         ],
-        redirectUrl: "./Moon/index.html"
+        redirectUrl: "../Meteorite/index.html"
     },
     {
         name: "Project Beta",
         description: [
             "Real-time data visualization platform"
         ],
-        redirectUrl: "./Moon/index.html"
+        redirectUrl: "../Meteorite/index.html"
     },
     {
         name: "Project Gamma",
         description: [
             "Distributed computing framework"
         ],
-        redirectUrl: "./Moon/index.html"
+        redirectUrl: "../Meteorite/index.html"
     },
     {
         name: "Project Delta",
         description: [
             "Cross-platform mobile development"
         ],
-        redirectUrl: "./Moon/index.html"
+        redirectUrl: "../Meteorite/index.html"
     }
 ];
